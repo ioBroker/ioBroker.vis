20892d95733a2210e6002226b9bde86a5d159d65
hobbyquaker <hobbyquaker@gmail.com>
2013-05-21 12:36:38 +0200

    neues Widget-Set: "fancyswitch"

--- dashui/js/dashui.js
+++ dashui/js/dashui.js
@@ -24,7 +24,7 @@
 // dui - the DashUI Engine
 var dui = {
 
-    version:            '0.6.7',
+    version:            '0.6.8',
     storageKeyViews:    'dashuiViews',
     storageKeySettings: 'dashuiSettings',
     storageKeyInstance: 'dashuiInstance',
