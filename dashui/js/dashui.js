c2797939049ae9508c18ed4f131274a52f09d6be
hobbyquaker <hobbyquaker@gmail.com>
2013-05-22 13:12:36 +0200

    Fehler behoben - Buttons waren nicht mehr editierbar

--- dashui/js/dashui.js
+++ dashui/js/dashui.js
@@ -24,7 +24,7 @@
 // dui - the DashUI Engine
 var dui = {
 
-    version:            '0.6.10',
+    version:            '0.6.11',
     storageKeyViews:    'dashuiViews',
     storageKeySettings: 'dashuiSettings',
     storageKeyInstance: 'dashuiInstance',
