d75a2d03eacf86a56d2f9e8a324d1c7f98b15b47
hobbyquaker <hobbyquaker@gmail.com>
2013-05-22 21:36:46 +0200

    neues Widget "basic - hm_ctrl - bool checkbox", Widget "basic - hm_val - hideOnFalse" gefixt

--- dashui/js/dashui.js
+++ dashui/js/dashui.js
@@ -24,7 +24,7 @@
 // dui - the DashUI Engine
 var dui = {
 
-    version:            '0.7.0',
+    version:            '0.7.1',
     storageKeyViews:    'dashuiViews',
     storageKeySettings: 'dashuiSettings',
     storageKeyInstance: 'dashuiInstance',
