314292c31fb02b68baada436668d161e1b60f6a4
hobbyquaker <hobbyquaker@gmail.com>
2013-05-25 04:21:27 +0200

    Knobs ausgearbeitet

--- dashui/js/dashui.js
+++ dashui/js/dashui.js
@@ -24,7 +24,7 @@
 // dui - the DashUI Engine
 var dui = {
 
-    version:            '0.7.6',
+    version:            '0.7.7',
     storageKeyViews:    'dashuiViews',
     storageKeySettings: 'dashuiSettings',
     storageKeyInstance: 'dashuiInstance',
