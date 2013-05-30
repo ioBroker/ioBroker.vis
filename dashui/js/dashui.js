be4c2ebc3b6ef0cfdd6b6499d2c5da620da81e90
hobbyquaker <hobbyquaker@gmail.com>
2013-05-30 10:30:57 +0200

    2 Dateien aus mfd-icons entfernt, evtl Verursacher von FTP Problemen

--- dashui/js/dashui.js
+++ dashui/js/dashui.js
@@ -24,7 +24,7 @@
 // dui - the DashUI Engine
 var dui = {
 
-    version:            '0.8.0',
+    version:            '0.8.1',
     storageKeyViews:    'dashuiViews',
     storageKeySettings: 'dashuiSettings',
     storageKeyInstance: 'dashuiInstance',
