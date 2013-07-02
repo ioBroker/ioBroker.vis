e37e8f8deb9cd088ca1c2055cb8f166ebc1037fe
hobbyquaker <hobbyquaker@gmail.com>
2013-07-02 17:34:19 +0200

    0.8.2

--- dashui/js/dashui.js
+++ dashui/js/dashui.js
@@ -24,7 +24,7 @@
 // dui - the DashUI Engine
 var dui = {
 
-    version:            '0.8.1',
+    version:            '0.8.2',
     storageKeyViews:    'dashuiViews',
     storageKeySettings: 'dashuiSettings',
     storageKeyInstance: 'dashuiInstance',
