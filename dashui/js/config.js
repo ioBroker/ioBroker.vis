841cb47dd67d58cfaf7677437acf6fcba4481899
Bluefox <dogafox@gmail.com>
2013-07-21 22:20:59 +0200

    config.js

---
+++ dashui/js/config.js
@@ -0,0 +1,9 @@
+var duiConfig = {
+    widgetSets:         ["basic","colorpicker","fancyswitch","hqWidgets","knob","jqplot","jqui","jqui-mfd","dev"],
+    currentLang:        "de",
+    useCache:           true,
+    fileViews:          '/usr/local/addons/dashui.views',
+    ccu:                "192.168.1.5",
+    ccuIoUrl:           undefined, //"http://raspberrypi:2100",
+    defaultHmInterval:  7500,
+};
