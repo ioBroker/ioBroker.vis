709d8bc8f366a937252207c31588f04a79b0322a
GermanBluefox <deniskhaev@gmail.com>
2013-08-09 12:41:53 +0200

    Update config.js

--- dashui/js/config.js
+++ dashui/js/config.js
@@ -1,8 +1,8 @@
 var duiConfig = {
-    widgetSets:         ["basic","colorpicker","fancyswitch", {name: "hqWidgets", edit: "hqWidgetsEdit"},"knob","jqplot","jqui","jqui-mfd","dev"],
+    widgetSets:         ["basic","colorpicker","fancyswitch", {name: "hqWidgets", edit: "hqWidgetsEdit"},"timeAndWeather",knob","jqplot","jqui","jqui-mfd","dev"],
     currentLang:        "de",
     useCache:           true,
-    fileViews:          'addons/dashui.views',
+    fileViews:          /usr/local/addons/dashui.views,
     ccu:                null,      // Hier IP Adresse von CCU: "192.168.1.100",
     ccuIoUrl:           undefined, // Hier (falls vorhanden) URL von CCU.IO Server: "http://rasppi:2100",
     defaultHmInterval:  7500,      // Update interval in ms
