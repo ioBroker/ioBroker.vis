cbd66219109f0c13c4cba79d1a8ab4d14f25fc2c
hobbyquaker <hobbyquaker@gmail.com>
2013-09-06 20:04:34 +0200

    updated version

--- dashui/js/dashuiEdit.js
+++ dashui/js/dashuiEdit.js
@@ -22,7 +22,7 @@
 
 // duiEdit - the DashUI Editor
 dui = $.extend(true, dui, {
-    editVersion:        '0.9dev12',
+    editVersion:        '0.9dev13',
     toolbox:            $("#dui_editor"),
     selectView:         $("#select_view"),
     activeWidget:       "",
