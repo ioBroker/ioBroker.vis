6d16d18cb69a9f144fa96d60f63e57cc4574cecf
hobbyquaker <hobbyquaker@gmail.com>
2013-10-15 21:06:58 +0200

    updated version and readme

--- dashui/js/dashui.js
+++ dashui/js/dashui.js
@@ -22,7 +22,7 @@
 
 var dui = {
 
-    version:            '0.9beta18',
+    version:            '0.9beta19',
     storageKeyViews:    'dashuiViews',
     storageKeySettings: 'dashuiSettings',
     storageKeyInstance: 'dashuiInstance',
--- dashui/js/dashuiEdit.js
+++ dashui/js/dashuiEdit.js
@@ -22,7 +22,7 @@
 
 // duiEdit - the DashUI Editor
 dui = $.extend(true, dui, {
-    editVersion:        '0.9beta18',
+    editVersion:        '0.9beta19',
     toolbox:            $("#dui_editor"),
     selectView:         $("#select_view"),
     activeWidget:       "",
