78ab734a98ed606ad5eb666f5cc6a76ec95144b0
hobbyquaker <hobbyquaker@gmail.com>
2013-11-22 16:56:32 +0100

    neue widgets für dwd-adapter, überflüssige dateien entfernt

--- dashui/js/config.js
+++ dashui/js/config.js
@@ -1,6 +1,6 @@
 var duiConfig = {
 
-    widgetSets:         ["basic","bars","colorpicker","fancyswitch","knob",{name: "hqWidgets", edit: "hqWidgetsEdit"},"jqplot","jqui","jqui-mfd","swipe","timeAndWeather","yr-adapter","dev"],
+    widgetSets:         ["basic","bars","colorpicker","fancyswitch","knob",{name: "hqWidgets", edit: "hqWidgetsEdit"},"jqplot","jqui","jqui-mfd","swipe","timeAndWeather","weather-adapter","dev"],
     currentLang:        "de"
 
 };
