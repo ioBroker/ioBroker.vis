9f31b1a28cc715b0f0f36b16fd0babc118e7a925
hobbyquaker <hobbyquaker@gmail.com>
2014-01-02 15:03:25 +0100

    updated version

--- dashui/js/dashui.js
+++ dashui/js/dashui.js
@@ -22,7 +22,7 @@
 
 var dui = {
 
-    version:                '0.9beta49',
+    version:                '0.9beta50',
     requiredCcuIoVersion:   '1.0.8',
     storageKeyViews:        'dashuiViews',
     storageKeySettings:     'dashuiSettings',
