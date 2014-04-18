2eb45b2c1766f171a9dbc7350a56ec65dc197b35
hobbyquaker <hobbyquaker@gmail.com>
2014-04-18 12:02:41 +0200

    updated version

--- dashui/js/dashui.js
+++ dashui/js/dashui.js
@@ -24,7 +24,7 @@
 
 var dui = {
 
-    version:                '0.9beta74',
+    version:                '0.9beta75',
     requiredServerVersion:  '1.0.28',
     storageKeyViews:        'dashuiViews',
     storageKeySettings:     'dashuiSettings',
--- dashui/js/dashuiEdit.js
+++ dashui/js/dashuiEdit.js
@@ -25,7 +25,7 @@
 "use strict";
 
 dui = $.extend(true, dui, {
-    editVersion:        '0.9beta74',
+    editVersion:        '0.9beta75',
     toolbox:            $("#dui_editor"),
     selectView:         $("#select_view"),
     activeWidget:       "",
