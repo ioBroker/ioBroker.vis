0a79fa5d0065bd43c731e5464aeaf250b9320084
smiling-Jack <steffen.schorling@googlemail.com>
2015-02-17 10:11:33 +0100

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,29 +1,28 @@
 var visConfig = {
-    widgetSets: [
-        //"basic",
-        //{name: "metro",       depends: ["basic"]},
-        //{name: "fancyswitch", depends: ["basic"]},
-        //"tabs",
-        //"jqplot",
-        //"timeAndWeather",
-        "plumb",
-        //{name: "jqui",        depends: ["basic"]},
-        //{name: "jqui-mfd",    depends: ["basic", "jqui"]},
-        //"weather-adapter"
-        /*,
-        "bars",
-        "colorpicker",
-
-        {name: "homematic", depends: ["basic", "jqui"]},
-        "knob",
-//        {name: "hqWidgets", edit: "hqWidgetsEdit"},
-        "jqplot",
-        {name: "lcars",    depends: ["basic"]},
-        "RGraph",
-        "special",
-        "swipe",
-        "table",
-        "dev"*/
-    ],
-    language: window.navigator.userLanguage || window.navigator.language
+widgetSets: [
+"basic",
+{name: "metro", depends: ["basic"]},
+{name: "fancyswitch", depends: ["basic"]},
+"tabs",
+"jqplot",
+"timeAndWeather",
+{name: "jqui", depends: ["basic"]},
+{name: "jqui-mfd", depends: ["basic", "jqui"]},
+"weather-adapter"
+//"plumb",
+/*,
+"bars",
+"colorpicker",
+{name: "homematic", depends: ["basic", "jqui"]},
+"knob",
+// {name: "hqWidgets", edit: "hqWidgetsEdit"},
+"jqplot",
+{name: "lcars", depends: ["basic"]},
+"RGraph",
+"special",
+"swipe",
+"table",
+"dev"*/
+],
+language: window.navigator.userLanguage || window.navigator.language
 };
