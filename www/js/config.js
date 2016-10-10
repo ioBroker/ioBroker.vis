38b86adec98e24c968aa75c6500005a2fb5726e0
GermanBluefox <dogafox@gmail.com>
2016-10-10 17:08:24 +0200

    (bluefox) clean config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,69 +1,14 @@
 var visConfig = {
     "widgetSets": [
-        "bars",
         "basic",
-        "colorpicker",
-        {
-            "name": "communicate",
-            "depends": []
-        },
-        "dev",
-        {
-            "name": "fancyswitch",
-            "depends": [
-                "basic"
-            ]
-        },
-        {
-            "name": "google-fonts",
-            "always": true
-        },
-        "hqwidgets",
         "jqplot",
-        {
-            "name": "jqui-mfd",
-            "depends": [
-                "basic",
-                "jqui"
-            ]
-        },
         {
             "name": "jqui",
             "depends": [
                 "basic"
             ]
         },
-        {
-            "name": "justgage",
-            "depends": []
-        },
-        {
-            "name": "keyboard",
-            "depends": []
-        },
-        "knob",
-        {
-            "name": "lcars",
-            "depends": [
-                "basic"
-            ]
-        },
-        {
-            "name": "metro",
-            "depends": [
-                "jqui-mfd",
-                "basic"
-            ]
-        },
-        "plumb",
-        "RGraph",
-        "special",
-        "swipe",
-        "tabs",
-        "timeandweather",
-        "vkb",
-        "weather-adapter",
-        "weather"
+        "tabs"
     ]
 };
 if (typeof exports != 'undefined') {
