5cb59e51cbace1ee769f98582e481794236898a3
GermanBluefox <dogafox@gmail.com>
2017-07-04 20:52:21 +0200

    add words

---
+++ admin/words.js
@@ -0,0 +1,21 @@
+systemDictionary = {
+    "vis adapter settings":  {"en": "vis adapter settings",  "de": "vis Adapter-Einstellungen",  "ru": "Настройки драйвера vis"},
+    "License:":              {"en": "License key:", "de": "License Key:", "ru": "Лицензионный ключ:"},
+    "Check license":         {"en": "Check license", "de": "Lizenz prüfen", "ru": "Проверить лицензию"},
+    "Service is offline. Please try later.":         {
+        "en": "Service is offline or no internet available. Please try later or check the internet connection.",
+        "de": "Service ist momentan nicht erreichbar oder keine Internetverbindung vorhanden. Bitte fersuchen Sie später noch ein mal oder prüfen Sie die Internetverbindung.",
+        "ru": "Сервис сейчас недоступен или нет интернет соединения. Попробуйте попозже или проверте интеренет соединение."
+    },
+    "instruction": {
+        "en": "To use <b>vis</b>, you must get the license key for it.<br>" +
+              "<b>vis</b> is free for private use, but for commercial use it has a price.<br>" +
+              "You can get the license on <a href='https://iobroker.net' target='_blank'>iobroker.net</a> after short registration.",
+        "de": "Man braucht eine Lizenz um <b>vis</b> zu nutzen.<br>" +
+              "<b>vis</b> ist kostenlos für private Nutzung. Nur für kommerzielle Gebrauch muss man für eine Lizenz bezahlen.<br>" +
+              "Man kann eine Lizenz auf <a href='https://iobroker.net' target='_blank'>iobroker.net</a> nach Registrierung bekommen.",
+        "ru": "Для пользования <b>vis</b> необходимо получить лицензионный ключ.<br>" +
+              "<b>vis</b> бесплатен для личного использования в некоммерческих целях. Для использования <b>vis</b> в коммерческих целях необходимо заплатить за лицензию.<br>" +
+              "Лицензионный ключ можно получить здесь <a href='https://iobroker.net' target='_blank'>iobroker.net</a> после регистрации."
+    }
+};
