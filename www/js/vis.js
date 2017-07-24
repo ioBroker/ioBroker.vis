185aca68e48e43382f4c9c298f5077a6a27edcf4
GermanBluefox <dogafox@gmail.com>
2017-07-24 18:48:13 +0200

    (bluefox) new version

--- www/js/vis.js
+++ www/js/vis.js
@@ -104,7 +104,7 @@ if (typeof systemLang !== 'undefined' && typeof cordova === 'undefined') {
 
 var vis;
 vis = {
-    version: '0.15.4',
+    version: '0.15.5',
     requiredServerVersion: '0.0.0',
 
     storageKeyViews:    'visViews',
