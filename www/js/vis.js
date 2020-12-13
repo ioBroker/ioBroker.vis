d8716ab898835b0fc7e759e63afa8c7673da01bc
GermanBluefox <dogafox@gmail.com>
2020-12-13 16:34:00 +0100

    Update versions

--- www/js/vis.js
+++ www/js/vis.js
@@ -274,7 +274,7 @@ if (typeof systemLang !== 'undefined' && typeof cordova === 'undefined') {
 }
 
 var vis = {
-    version: '1.3.4',
+    version: '1.3.5',
     requiredServerVersion: '0.0.0',
 
     storageKeyViews:    'visViews',
