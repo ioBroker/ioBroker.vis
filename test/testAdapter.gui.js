0dc8cd4620eb697f803882774cbed064345224e2
GermanBluefox <dogafox@gmail.com>
2023-12-15 11:33:08 +0100

    Implemented GUI test

--- test/testAdapter.gui.js
+++ test/testAdapter.gui.js
@@ -1,4 +1,5 @@
 const helper = require('@iobroker/vis-2-widgets-testing');
+const fs = require('node:fs');
 
 let gPage;
 let gBrowser;
@@ -11,14 +12,24 @@ describe('vis', () => {
         await helper.startIoBroker({
             startOwnAdapter: true,
             additionalAdapters: ['web'],
-            visUploadedId: 'vis.0.info.uploaded',
             mainGuiProject: 'vis',
         });
         const { browser, page } = await helper.startBrowser(process.env.CI === 'true');
         gBrowser = browser
         gPage = page
-        // await helper.createProject();
 
+        gPage.on('dialog', async dialog => {
+            console.log('here');
+            await dialog.accept();
+        });
+        // await helper.createProject();
+        gPage.goto(`http://localhost:18082/vis/index.html`, { waitUntil: 'domcontentloaded' });
+
+        // create screenshots directory
+        !fs.existsSync(`${__dirname}/../tmp/screenshots`) && fs.mkdirSync(`${__dirname}/../tmp/screenshots`);
+
+        await new Promise(resolve => setTimeout(resolve, 5_000));
+        await gPage.waitForSelector('#menu_body', { timeout: 5_000 });
         // open widgets
         // await helper.palette.openWidgetSet(gPage, 'basic');
         await helper.screenshot(gPage, '02_widgets_opened');
@@ -26,19 +37,13 @@ describe('vis', () => {
 
     it('Check runtime', async function (){
         this.timeout(20_000);
-        // add widget in editor
-        const basicWidgets = await helper.palette.getListOfWidgets(gPage, 'basic');
-        // const wid = await helper.palette.addWidget(gPage, basicWidgets[0], true);
-        // wait for saving
-        // await new Promise(resolve => setTimeout(resolve, 5_000));
-
         await helper.screenshot(gPage, '90_runtime');
 
         const runtimePage = await gBrowser.newPage();
 
         // open runtime
         await runtimePage.goto(`http://127.0.0.1:18082/vis/index.html`, { waitUntil: 'domcontentloaded' });
-        await runtimePage.waitForSelector('#root', { timeout: 5_000 });
+        await runtimePage.waitForSelector('#vis_container', { timeout: 15_000 });
         // await runtimePage.waitForSelector(`#${wid}`, { timeout: 20_000 });
         await helper.screenshot(runtimePage, '91_runtime');
 
