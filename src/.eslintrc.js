8cd08f4be4c9c907cfd5738d77a5b8c3a67eedd9
foxriver76 <moritz.heusinger@gmail.com>
2024-01-03 08:56:49 +0100

    add ioBroker to eslint globals

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
         browser: true,
         es2021: true,
     },
+    globals: {
+        ioBroker: true,
+    },
     extends: [
         'eslint:recommended',
         'plugin:react/recommended',
