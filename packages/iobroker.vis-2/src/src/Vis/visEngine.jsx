1a92b1c01225286c19dc7a9a780d40c9f3c750cd
oweitman <oweitman@gmx.de>
2024-06-13 17:38:26 +0200

    add subscribe/unsubscribe command to the vis object, that widgets can subscribe/unsubscribe datapoint by themselve

--- packages/iobroker.vis-2/src/src/Vis/visEngine.jsx
+++ packages/iobroker.vis-2/src/src/Vis/visEngine.jsx
@@ -474,6 +474,8 @@ class VisEngine extends React.Component {
                 return false;
             },
             setValue: this.setValue,
+            subscribe: this.subscribe,
+            unsubscribe: this.unsubscribe,
             changeView: (viewDiv, view, hideOptions, showOptions, sync, cb) => {
                 if (typeof view === 'object') {
                     cb = sync;
