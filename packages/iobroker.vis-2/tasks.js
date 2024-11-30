7134cb3be27e21b1d735729f34aee5638c0138a2
GermanBluefox <dogafox@gmail.com>
2024-11-30 18:20:04 +0000

    Rename vis folder to src-vis

--- packages/iobroker.vis-2/tasks.js
+++ packages/iobroker.vis-2/tasks.js
@@ -15,13 +15,13 @@ function clean() {
 
 function copyRuntimeSrc() {
     !fs.existsSync(`${__dirname}/runtime`) && fs.mkdirSync(`${__dirname}/runtime`);
-    !fs.existsSync(`${__dirname}/runtime/src`) && fs.mkdirSync(`${__dirname}/runtime/src`);
+    !fs.existsSync(`${__dirname}/runtime/src-vis`) && fs.mkdirSync(`${__dirname}/runtime/src`);
     // copy only single shared utils file now
-    !fs.existsSync(`${__dirname}/runtime/src/Vis`) && fs.mkdirSync(`${__dirname}/runtime/src/Vis`);
-    !fs.existsSync(`${__dirname}/runtime/src/Utils`) && fs.mkdirSync(`${__dirname}/runtime/src/Utils`);
-    !fs.existsSync(`${__dirname}/runtime/src/i18n`) && fs.mkdirSync(`${__dirname}/runtime/src/i18n`);
+    !fs.existsSync(`${__dirname}/runtime/src-vis/Vis`) && fs.mkdirSync(`${__dirname}/runtime/src/Vis`);
+    !fs.existsSync(`${__dirname}/runtime/src-vis/Utils`) && fs.mkdirSync(`${__dirname}/runtime/src/Utils`);
+    !fs.existsSync(`${__dirname}/runtime/src-vis/i18n`) && fs.mkdirSync(`${__dirname}/runtime/src/i18n`);
     !fs.existsSync(`${__dirname}/runtime/public`) && fs.mkdirSync(`${__dirname}/runtime/public`);
-    copyFolder(`${__dirname}/src/public`, `${__dirname}/runtime/public`, ['ace', 'visEditWords.js']);
+    copyFolder(`${__dirname}/src-vis/public`, `${__dirname}/runtime/public`, ['ace', 'visEditWords.js']);
     let text = fs.readFileSync(`${__dirname}/runtime/public/index.html`).toString('utf-8');
     let runtimeText = text.replace('<title>Editor.vis</title>', '<title>ioBroker.vis</title>');
     runtimeText = runtimeText.replace('faviconEdit.ico', 'favicon.ico');
@@ -29,13 +29,13 @@ function copyRuntimeSrc() {
         fs.writeFileSync(`${__dirname}/runtime/public/index.html`, runtimeText);
     }
 
-    copyFolder(`${__dirname}/src/src/Vis`, `${__dirname}/runtime/src/Vis`, [
+    copyFolder(`${__dirname}/src-vis/src/Vis`, `${__dirname}/runtime/src/Vis`, [
         'visContextMenu.tsx',
         'oldVis.jsx',
         'visOrderMenu.tsx',
         'BulkEditor.tsx',
     ]);
-    copyFolder(`${__dirname}/src/src/img`, `${__dirname}/runtime/src/img`);
+    copyFolder(`${__dirname}/src-vis/src/img`, `${__dirname}/runtime/src/img`);
 
     fs.writeFileSync(
         `${__dirname}/runtime/src/Vis/visOrderMenu.tsx`,
@@ -89,7 +89,7 @@ export default FiltersEditorDialog;
 `,
     );
 
-    const pack = JSON.parse(fs.readFileSync(`${__dirname}/src/package.json`).toString());
+    const pack = JSON.parse(fs.readFileSync(`${__dirname}/src-vis/package.json`).toString());
     delete pack.dependencies['@devbookhq/splitter'];
     delete pack.dependencies['ace-builds'];
     delete pack.dependencies['iobroker.type-detector'];
@@ -104,32 +104,32 @@ export default FiltersEditorDialog;
     delete pack.dependencies['html-to-image'];
 
     fs.writeFileSync(`${__dirname}/runtime/package.json`, JSON.stringify(pack, null, 2));
-    fs.writeFileSync(`${__dirname}/runtime/craco.config.js`, fs.readFileSync(`${__dirname}/src/craco.config.js`));
+    fs.writeFileSync(`${__dirname}/runtime/craco.config.js`, fs.readFileSync(`${__dirname}/src-vis/craco.config.js`));
     fs.writeFileSync(
         `${__dirname}/runtime/modulefederation.config.js`,
-        fs.readFileSync(`${__dirname}/src/modulefederation.config.js`),
+        fs.readFileSync(`${__dirname}/src-vis/modulefederation.config.js`),
     );
-    fs.writeFileSync(`${__dirname}/runtime/src/Editor.tsx`, fs.readFileSync(`${__dirname}/src/src/Runtime.tsx`));
-    fs.writeFileSync(`${__dirname}/runtime/src/version.json`, fs.readFileSync(`${__dirname}/src/src/version.json`));
-    fs.writeFileSync(`${__dirname}/runtime/tsconfig.json`, fs.readFileSync(`${__dirname}/src/tsconfig.json`));
-    fs.writeFileSync(`${__dirname}/runtime/src/Store.tsx`, fs.readFileSync(`${__dirname}/src/src/Store.tsx`));
+    fs.writeFileSync(`${__dirname}/runtime/src/Editor.tsx`, fs.readFileSync(`${__dirname}/src-vis/src/Runtime.tsx`));
+    fs.writeFileSync(`${__dirname}/runtime/src/version.json`, fs.readFileSync(`${__dirname}/src-vis/src/version.json`));
+    fs.writeFileSync(`${__dirname}/runtime/tsconfig.json`, fs.readFileSync(`${__dirname}/src-vis/tsconfig.json`));
+    fs.writeFileSync(`${__dirname}/runtime/src/Store.tsx`, fs.readFileSync(`${__dirname}/src-vis/src/Store.tsx`));
     fs.writeFileSync(
         `${__dirname}/runtime/src/Utils/utils.tsx`,
-        fs.readFileSync(`${__dirname}/src/src/Utils/utils.tsx`),
+        fs.readFileSync(`${__dirname}/src-vis/src/Utils/utils.tsx`),
     );
     fs.writeFileSync(
         `${__dirname}/runtime/src/serviceWorker.tsx`,
-        fs.readFileSync(`${__dirname}/src/src/serviceWorker.tsx`),
+        fs.readFileSync(`${__dirname}/src-vis/src/serviceWorker.tsx`),
     );
-    fs.writeFileSync(`${__dirname}/runtime/src/index.tsx`, fs.readFileSync(`${__dirname}/src/src/index.tsx`));
-    fs.writeFileSync(`${__dirname}/runtime/src/theme.tsx`, fs.readFileSync(`${__dirname}/src/src/theme.tsx`));
-    fs.writeFileSync(`${__dirname}/runtime/src/bootstrap.tsx`, fs.readFileSync(`${__dirname}/src/src/bootstrap.tsx`));
-    fs.writeFileSync(`${__dirname}/runtime/src/index.css`, fs.readFileSync(`${__dirname}/src/src/index.css`));
+    fs.writeFileSync(`${__dirname}/runtime/src/index.tsx`, fs.readFileSync(`${__dirname}/src-vis/src/index.tsx`));
+    fs.writeFileSync(`${__dirname}/runtime/src/theme.tsx`, fs.readFileSync(`${__dirname}/src-vis/src/theme.tsx`));
+    fs.writeFileSync(`${__dirname}/runtime/src/bootstrap.tsx`, fs.readFileSync(`${__dirname}/src-vis/src/bootstrap.tsx`));
+    fs.writeFileSync(`${__dirname}/runtime/src/index.css`, fs.readFileSync(`${__dirname}/src-vis/src/index.css`));
     fs.writeFileSync(
         `${__dirname}/runtime/src/Utils/styles.tsx`,
         'const commonStyles: Record<string, any> = {};\nexport default commonStyles;',
     );
-    copyFolder(`${__dirname}/src/src/i18nRuntime`, `${__dirname}/runtime/src/i18n`);
+    copyFolder(`${__dirname}/src-vis/src/i18nRuntime`, `${__dirname}/runtime/src/i18n`);
 }
 
 function copyRuntimeDist() {
@@ -154,9 +154,9 @@ async function generateSvgFiles() {
     const svgPath = path.join(rootDir, '/node_modules/@material-icons/svg/');
     const data = JSON.parse(fs.readFileSync(`${svgPath}data.json`).toString('utf8'));
 
-    !fs.existsSync(`${__dirname}/src/public/material-icons`) && fs.mkdirSync(`${__dirname}/src/public/material-icons`);
+    !fs.existsSync(`${__dirname}/src-vis/public/material-icons`) && fs.mkdirSync(`${__dirname}/src-vis/public/material-icons`);
 
-    updateFile(`${__dirname}/src/public/material-icons/index.json`, JSON.stringify(data.icons));
+    updateFile(`${__dirname}/src-vis/public/material-icons/index.json`, JSON.stringify(data.icons));
 
     const folders = fs.readdirSync(`${svgPath}svg`);
     const result = {};
@@ -180,7 +180,7 @@ async function generateSvgFiles() {
 
     Object.keys(result).forEach(file => {
         updateFile(
-            `${__dirname}/src/public/material-icons/${file.replace('.svg', '')}.json`,
+            `${__dirname}/src-vis/public/material-icons/${file.replace('.svg', '')}.json`,
             JSON.stringify(result[file]),
         );
     });
@@ -232,7 +232,7 @@ async function generateSvgFiles() {
             result[files[f].replace('.svg', '')] = Buffer.from(data).toString('base64');
         }
 
-        updateFile(`${__dirname}/src/public/material-icons/knx-uf.json`, JSON.stringify(result));
+        updateFile(`${__dirname}/src-vis/public/material-icons/knx-uf.json`, JSON.stringify(result));
     }
 }
 
@@ -252,45 +252,45 @@ function syncFiles(target, dest) {
 }
 
 function buildEditor() {
-    // copy ace files into src/public/lib/js/ace
+    // copy ace files into src-vis/public/lib/js/ace
     let ace = `${rootDir}/node_modules/ace-builds/src-min-noconflict/`;
-    syncFiles(`${__dirname}/src/public/lib/js/ace/worker-html.js`, `${ace}worker-html.js`);
-    syncFiles(`${__dirname}/src/public/lib/js/ace/mode-html.js`, `${ace}mode-html.js`);
-    syncFiles(`${__dirname}/src/public/lib/js/ace/snippets/html.js`, `${ace}snippets/html.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/worker-html.js`, `${ace}worker-html.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/mode-html.js`, `${ace}mode-html.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/snippets/html.js`, `${ace}snippets/html.js`);
 
-    syncFiles(`${__dirname}/src/public/lib/js/ace/worker-css.js`, `${ace}worker-css.js`);
-    syncFiles(`${__dirname}/src/public/lib/js/ace/mode-css.js`, `${ace}mode-css.js`);
-    syncFiles(`${__dirname}/src/public/lib/js/ace/snippets/css.js`, `${ace}snippets/css.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/worker-css.js`, `${ace}worker-css.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/mode-css.js`, `${ace}mode-css.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/snippets/css.js`, `${ace}snippets/css.js`);
 
-    syncFiles(`${__dirname}/src/public/lib/js/ace/mode-json.js`, `${ace}mode-json.js`);
-    syncFiles(`${__dirname}/src/public/lib/js/ace/worker-json.js`, `${ace}worker-json.js`);
-    syncFiles(`${__dirname}/src/public/lib/js/ace/snippets/json.js`, `${ace}snippets/json.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/mode-json.js`, `${ace}mode-json.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/worker-json.js`, `${ace}worker-json.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/snippets/json.js`, `${ace}snippets/json.js`);
 
-    syncFiles(`${__dirname}/src/public/lib/js/ace/mode-javascript.js`, `${ace}mode-javascript.js`);
-    syncFiles(`${__dirname}/src/public/lib/js/ace/worker-javascript.js`, `${ace}worker-javascript.js`);
-    syncFiles(`${__dirname}/src/public/lib/js/ace/snippets/javascript.js`, `${ace}snippets/javascript.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/mode-javascript.js`, `${ace}mode-javascript.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/worker-javascript.js`, `${ace}worker-javascript.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/snippets/javascript.js`, `${ace}snippets/javascript.js`);
 
-    syncFiles(`${__dirname}/src/public/lib/js/ace/ext-language_tools.js`, `${ace}ext-language_tools.js`);
-    syncFiles(`${__dirname}/src/public/lib/js/ace/ext-searchbox.js`, `${ace}ext-searchbox.js`);
-    syncFiles(`${__dirname}/src/public/lib/js/ace/theme-clouds_midnight.js`, `${ace}theme-clouds_midnight.js`);
-    syncFiles(`${__dirname}/src/public/lib/js/ace/theme-chrome.js`, `${ace}theme-chrome.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/ext-language_tools.js`, `${ace}ext-language_tools.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/ext-searchbox.js`, `${ace}ext-searchbox.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/theme-clouds_midnight.js`, `${ace}theme-clouds_midnight.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/theme-chrome.js`, `${ace}theme-chrome.js`);
 
-    syncFiles(`${__dirname}/src/public/lib/js/ace/ace.js`, `${ace}ace.js`);
+    syncFiles(`${__dirname}/src-vis/public/lib/js/ace/ace.js`, `${ace}ace.js`);
 
     // synchronise i18n: copy all new words from runtime into src
     const langsRuntime = {
-        en: require('./src/src/i18nRuntime/en.json'),
+        en: require('./src-vis/src/i18nRuntime/en.json'),
     };
     const langsEditor = {
-        en: require('./src/src/i18n/en.json'),
+        en: require('./src-vis/src/i18n/en.json'),
     };
     Object.keys(langsRuntime.en).forEach(key => {
         if (!langsEditor.en[key]) {
             // load all languages
             if (!langsEditor.de) {
-                fs.readdirSync(`${__dirname}/src/src/i18nRuntime`).forEach(file => {
-                    langsRuntime[file.replace('.json', '')] = require(`./src/src/i18nRuntime/${file}`);
-                    langsEditor[file.replace('.json', '')] = require(`./src/src/i18n/${file}`);
+                fs.readdirSync(`${__dirname}/src-vis/src/i18nRuntime`).forEach(file => {
+                    langsRuntime[file.replace('.json', '')] = require(`./src-vis/src/i18nRuntime/${file}`);
+                    langsEditor[file.replace('.json', '')] = require(`./src-vis/src/i18n/${file}`);
                 });
             }
             Object.keys(langsEditor).forEach(lang => (langsEditor[lang][key] = langsRuntime[lang][key]));
@@ -299,18 +299,18 @@ function buildEditor() {
 
     if (langsEditor.de) {
         Object.keys(langsEditor).forEach(lang =>
-            fs.writeFileSync(`${__dirname}/src/src/i18n/${lang}.json`, JSON.stringify(langsEditor[lang], null, 2)),
+            fs.writeFileSync(`${__dirname}/src-vis/src/i18n/${lang}.json`, JSON.stringify(langsEditor[lang], null, 2)),
         );
     }
 
-    return buildReact(`${__dirname}/src/`, { craco: true, ramSize: 7000, rootDir: `${__dirname}/../../` });
+    return buildReact(`${__dirname}/src-vis/`, { craco: true, ramSize: 7000, rootDir: `${__dirname}/../../` });
 }
 
 function copyAllFiles() {
-    copyFolder(path.join(__dirname, 'src/build'), path.join(__dirname, 'www'), ['index.html']);
+    copyFolder(path.join(__dirname, 'src-vis/build'), path.join(__dirname, 'www'), ['index.html']);
     fs.writeFileSync(
         path.join(__dirname, 'www/edit.html'),
-        fs.readFileSync(path.join(__dirname, 'src', 'build', 'index.html')),
+        fs.readFileSync(path.join(__dirname, 'src-vis', 'build', 'index.html')),
     );
     return Promise.resolve();
 }
@@ -364,8 +364,8 @@ function copyFolder(source, target, ignore) {
 function patchEditor() {
     patchFile(`${__dirname}/www/edit.html`);
     patchFile(`${__dirname}/www/index.html`);
-    patchFile(`${__dirname}/src/build/index.html`);
-    patchFile(`${__dirname}/src/build/edit.html`);
+    patchFile(`${__dirname}/src-vis/build/index.html`);
+    patchFile(`${__dirname}/src-vis/build/edit.html`);
     fs.existsSync(`${__dirname}/www/marketplaceConfig.sample.js`) &&
         fs.unlinkSync(`${__dirname}/www/marketplaceConfig.sample.js`);
     copyFolder(`${__dirname}/www`, `${__dirname}/../../www`);
@@ -393,10 +393,10 @@ if (process.argv.includes('--runtime-0-clean')) {
 } else if (process.argv.includes('--runtime-5-patch')) {
     patchRuntime();
 } else if (process.argv.includes('--0-clean')) {
-    deleteFoldersRecursive(`${__dirname}/src/build`);
+    deleteFoldersRecursive(`${__dirname}/src-vis/build`);
 } else if (process.argv.includes('--1-npm')) {
-    if (!fs.existsSync(`${__dirname}/src/node_modules`)) {
-        npmInstall(`${__dirname}/src`).catch(e => console.error(`Cannot install: ${e}`));
+    if (!fs.existsSync(`${__dirname}/src-vis/node_modules`)) {
+        npmInstall(`${__dirname}/src-vis`).catch(e => console.error(`Cannot install: ${e}`));
     }
 } else if (process.argv.includes('--2-svg-icons')) {
     generateSvgFiles().catch(e => console.error(`Cannot generate SVG icons: ${e}`));
@@ -413,10 +413,10 @@ if (process.argv.includes('--runtime-0-clean')) {
         .then(() => buildReact(`${__dirname}/runtime/`, { craco: true, ramSize: 7000, rootDir: `${__dirname}/../../` }))
         .then(() => copyRuntimeDist())
         .then(() => patchRuntime())
-        .then(() => deleteFoldersRecursive(`${__dirname}/src/build`))
+        .then(() => deleteFoldersRecursive(`${__dirname}/src-vis/build`))
         .then(() => {
-            if (!fs.existsSync(`${__dirname}/src/node_modules`)) {
-                return npmInstall(`${__dirname}/src`);
+            if (!fs.existsSync(`${__dirname}/src-vis/node_modules`)) {
+                return npmInstall(`${__dirname}/src-vis`);
             }
             return Promise.resolve();
         })
