de06f7e8eb723cd6d80a227a562c5f86115d1d39
dilesoft <dilesoft@gmail.com>
2025-03-22 12:47:14 +0300

    vite typescript

---
+++ packages/iobroker.vis-2/src-vis/vite.config.ts
@@ -0,0 +1,51 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import commonjs from 'vite-plugin-commonjs';
+import vitetsConfigPaths from 'vite-tsconfig-paths';
+import { federation } from '@module-federation/vite';
+import path, { resolve } from 'node:path';
+// import { moduleFederationShared } from '@iobroker/adapter-react-v5/modulefederation.admin.config';
+
+export default defineConfig({
+    plugins: [
+        federation({
+            name: 'iobroker_vis',
+            shared: [],
+            // shared: moduleFederationShared(),
+            exposes: {
+                './visRxWidget': './src/Vis/visRxWidget',
+            },
+            remotes: {},
+            filename: 'remoteEntry.js',
+            manifest: true,
+        }),
+        react(),
+        vitetsConfigPaths(),
+        commonjs(),
+    ],
+    server: {
+        port: 3000,
+        proxy: {
+            '/_socket': 'http://localhost:8082',
+            '/vis-2.0': 'http://localhost:8082',
+            '/adapter': 'http://localhost:8082',
+            '/habpanel': 'http://localhost:8082',
+            '/vis-2': 'http://localhost:8082',
+            '/widgets': 'http://localhost:8082/vis-2',
+            '/widgets.html': 'http://localhost:8082/vis-2',
+            '/web': 'http://localhost:8082',
+            '/state': 'http://localhost:8082',
+        },
+    },
+    base: './',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@iobroker/types-vis-2': path.resolve(__dirname, '..', '..', 'types-vis-2'),
+        },
+    },
+    build: {
+        target: 'chrome89',
+        outDir: './build',
+    },
+});
